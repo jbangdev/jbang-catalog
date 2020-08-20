fe70fec95cde5fc6ece32c3f1126983b14437d5d
Max Rydahl Andersen <max@xam.dk>
2020-08-20 16:17:07 +0200

    upgrade to latest consoleui

--- gavsearch.java
+++ gavsearch.java
@@ -3,7 +3,7 @@
 //DEPS info.picocli:picocli:4.2.0
 //DEPS org.jboss.resteasy:resteasy-client:4.5.6.Final
 //DEPS org.jboss.resteasy:resteasy-jackson2-provider:4.5.6.Final
-//DEPS de.codeshelf.consoleui:consoleui:0.0.11
+//DEPS de.codeshelf.consoleui:consoleui:0.0.13
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import de.codeshelf.consoleui.prompt.ConsolePrompt;
@@ -25,6 +25,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import static java.lang.System.out;
 import static org.fusesource.jansi.Ansi.ansi;
@@ -36,6 +38,9 @@ public class gavsearch implements Callable<Integer> {
     @Parameters(index = "0", description = "Query string to use when searching search.maven.org")
     private String query;
 
+    @CommandLine.Option(names={ "--formats", " -f" }, split =",", defaultValue="maven,gradle,jbang")
+    private List<String> formats;
+
     public static void main(String... args) {
         int exitCode = new CommandLine(new gavsearch()).execute(args);
         System.exit(exitCode);
@@ -44,6 +49,12 @@ public class gavsearch implements Callable<Integer> {
     @Override
     public Integer call() throws Exception { // your business logic goes here...
         AnsiConsole.systemInstall();
+
+        Map<String, Function<Doc, String>> formatMap = new HashMap<>();
+        formatMap.put("gradle", Doc::gradle);
+        formatMap.put("maven", Doc::maven);
+        formatMap.put("jbang", Doc::jbang);
+
 
         var client = ClientBuilder.newClient();
         try(var thing = client.target("https://search.maven.org/solrsearch/select?rows=100&q=" + URLEncoder.encode(query, "UTF-8")).request().get()) {
@@ -70,8 +81,15 @@ public class gavsearch implements Callable<Integer> {
                 String selid = ((ListResult)response.get("gav")).getSelectedId();
                 Doc selected = gavmap.get(selid);
 
-                out.println(ansi().render("@|bold gradle:|@\n") + selected.gradle());
-                out.println(ansi().render("@|bold maven:|@ \n") + selected.maven());
+                formats.forEach(s -> {
+                    var f = formatMap.get(s);
+                    if(f!=null) {
+                        out.println(ansi().render("@|bold " + s + ":|@\n") +
+                                f.apply(selected));
+                    } else {
+                        System.err.println("Unknown format: " + s);
+                    }
+                });
 
                 /*builder = new ConsolePrompt().getPromptBuilder();
                 builder.createListPrompt()
@@ -130,6 +148,9 @@ public class gavsearch implements Callable<Integer> {
             return g + ":" + a + ":" + latestVersion;
         }
 
+        public String jbang() {
+            return "//DEPS " + gradle();
+        }
         public String maven() {
             return "<dependency>\n" +
                     "  <groupId>" + g + "</groupId>\n" +
