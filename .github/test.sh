d52a1082cfdf5bade4d85603ad188c5fceb83613
Max Rydahl Andersen <max.andersen@gmail.com>
2023-10-19 21:29:18 +0200

    fix deps

--- .github/test.sh
+++ .github/test.sh
@@ -2,9 +2,12 @@
 
 has_error=0
 
-jq -r '.aliases[]["script-ref"] | select(contains(".java"))' jbang-catalog.json | while read -r file; do 
-    jbang build "$file" || { echo "Error building $file"; has_error=1; }
-done
+while read -r file; do 
+    if ! jbang build "$file"; then
+        echo "Error building $file"
+        has_error=1
+    fi
+done < <(jq -r '.aliases[]["script-ref"] | select(contains(".java"))' jbang-catalog.json)
 
 echo "has_error: $has_error"
 exit $has_error
