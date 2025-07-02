10e62c302a0276d20e4f4162c74f2d8059c2c992
Werner Fouché <werner.fouche@gmail.com>
2025-07-02 11:05:29 +0200

    Use the correct jbang launch script to run commands

--- deps.java
+++ deps.java
@@ -14,7 +14,8 @@ public class deps {
     public static void main(String... args) throws Exception {
         if (args.length > 0) {
             String script = args[0];
-            String dependencies = $("jbang info tools --select=dependencies " + script).get();
+            String jbang_launch_cmd = System.getenv("JBANG_LAUNCH_CMD");
+            String dependencies = $(jbang_launch_cmd + " info tools --select=dependencies " + script).get();
             JsonNode deps = new ObjectMapper().readTree(dependencies);
             List<String> gavList = new LinkedList<>();
             deps.forEach(
