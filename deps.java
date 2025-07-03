808a5e9554a676d0313eee30bf51c82be8d6e910
Werner Fouché <werner.fouche@gmail.com>
2025-07-03 17:17:44 +0200

    Comment about //DEPS eu.maveniverse.maven.plugins:toolbox:<version>

--- deps.java
+++ deps.java
@@ -3,8 +3,13 @@
 //DEPS dev.jbang:jash:0.0.3
 //DEPS com.fasterxml.jackson.core:jackson-databind:2.19.1
 //DEPS info.picocli:picocli:4.7.7
+//DEPS eu.maveniverse.maven.plugins:toolbox:0.11.2
 
-// Future Use Dependency --> eu.maveniverse.maven.plugins:toolbox:0.11.2
+// NOTE: The eu.maveniverse.maven.plugins:toolbox dependency is not used directly.
+// We only include it here so that running deps.java on itself will let us know if
+// a newer version of toolbox is available. If a newer toolbox is available, then
+// update the  URL pointing to the toolbox jar. See String variable toolboxJar.
+// Also update //DEPS eu.maveniverse.maven.plugins:toolbox:<version>
 
 import static dev.jbang.jash.Jash.*;
 
@@ -37,7 +42,7 @@ public class deps implements Callable<Integer> {
         if (script.equals("")) {
             return 1;
         }
-        String toolboxJar = "https://repo.maven.apache.org/maven2/eu/maveniverse/maven/plugins/toolbox/0.11.2/toolbox-0.11.2-cli.jar ";
+        String toolboxJar = "https://repo.maven.apache.org/maven2/eu/maveniverse/maven/plugins/toolbox/0.11.2/toolbox-0.11.2-cli.jar";
         String jbang_launch_cmd = System.getenv("JBANG_LAUNCH_CMD");
         String dependencies = $(jbang_launch_cmd + " info tools --quiet --select=dependencies " + script).get();
         JsonNode deps = new ObjectMapper().readTree(dependencies);
